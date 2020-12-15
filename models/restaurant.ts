0f8d8ace6355cb19f1e96621050f3fa0eee2e676
tomporat247 <tomporat247@gmail.com>
2020-12-15 23:32:30 +0200

    add restaurant doc

--- models/restaurant.ts
+++ models/restaurant.ts
@@ -18,3 +18,55 @@ export type CreateRestaurantBody = Omit<Omit<Restaurant, '_id'>, 'reviews'>;
 export type UpdateRestaurantBody = Partial<CreateRestaurantBody>;
 
 export interface RestaurantDocument extends Restaurant, Document {}
+
+// TODO: Add reviews
+/**
+ * @swagger
+ *  components:
+ *    schemas:
+ *      Restaurant:
+ *        type: object
+ *        required:
+ *          - _id
+ *          - name
+ *          - description
+ *          - rating
+ *          - address
+ *          - imageUrl
+ *          - category
+ *        properties:
+ *          _id:
+ *            type: string
+ *            format: uuid
+ *          name:
+ *            type: string
+ *          description:
+ *            type: string
+ *          rating:
+ *            type: number
+ *          address:
+ *            type: object
+ *            properties:
+ *              city:
+ *                type: string
+ *              street:
+ *                type: string
+ *              houseNumber:
+ *                type: number
+ *            required:
+ *              - city
+ *              - street
+ *              - houseNumber
+ *          imageUrl:
+ *            type: string
+ *          category:
+ *            $ref: '#/components/schemas/Category'
+ *        example:
+ *           _id: "xxx"
+ *           name: KFC
+ *           description: fried chicken goodies
+ *           rating: 10
+ *           imageUrl: http://some-link
+ *           category: some category
+ *           address: {city: 'Tel Aviv', street: 'Hashalom', houseNumber: 1}
+ */
