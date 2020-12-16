5ac91bd2032db803933d1e05eb6d0ac3c20409f0
tomporat247 <tomporat247@gmail.com>
2020-12-17 00:05:32 +0200

    remove unnecessary function call

--- config/init.ts
+++ config/init.ts
@@ -2,7 +2,7 @@ import * as nconf from 'nconf';
 import { resolve } from 'path';
 
 const configDirectoryPath =
-  process.env.NODE_ENV === 'production' ? resolve(__dirname, '../../', 'config') : resolve(__dirname);
+  process.env.NODE_ENV === 'production' ? resolve(__dirname, '../../', 'config') : __dirname;
 
 nconf
   .argv()
