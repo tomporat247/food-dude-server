e56cd0647eb1c654c833d69e100b758945b02872
tomporat247 <tomporat247@gmail.com>
2020-12-18 22:37:33 +0200

    remove get current user

--- src/routes/user.ts
+++ src/routes/user.ts
@@ -1,6 +1,6 @@
 import { Router } from 'express';
 import { isAdminMiddleWare, isAdminOrCurrentUserMiddleware } from '../middlewares/auth';
-import { getCurrentUser, getUsers, removeUser, updateUser } from '../controllers/user';
+import { getUsers, removeUser, updateUser } from '../controllers/user';
 import { validateUserUpdateBody } from '../middlewares/validators/user-validator';
 import { getParameterObjectIdValidator } from '../middlewares/validators/common-validators';
 
@@ -8,7 +8,6 @@ export const userRouter = Router();
 
 //@ts-ignore
 userRouter.get('/', isAdminMiddleWare, getUsers);
-userRouter.get('/current', getCurrentUser);
 //@ts-ignore
 userRouter.delete('/:id', isAdminMiddleWare, getParameterObjectIdValidator('id'), removeUser);
 userRouter.put(
