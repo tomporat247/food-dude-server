b781576efcfbab77f9cdcc189f6dfcd9f1b00b2b
tomporat247 <tomporat247@gmail.com>
2021-01-01 17:51:24 +0200

    remvoe console.log

--- src/middlewares/session.ts
+++ src/middlewares/session.ts
@@ -1,8 +1,6 @@
 import * as session from 'express-session';
 import { get } from 'nconf';
 
-console.log(get('auth:secret'));
-
 export const sessionMiddleWare = session({
   secret: get('auth:secret'),
   resave: false,
